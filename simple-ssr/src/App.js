5ac6a888f31fe0b980b2760e372368bde15c06ae
Yifeng Zhu <yifeng.zhu.mel@gmail.com>
2022-07-19 20:20:28 +1000

    lambdaonly

--- simple-ssr/src/App.js
+++ simple-ssr/src/App.js
@@ -1,35 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import React, { useEffect, useState } from "react";
-import ProductList from "./components/ProductList";
-import config from "./config.json";
-import axios from "axios";
+import React from "react";
+import LearnBetterLife from "./components/LearnBetterLife";
 
 const App = ({ isSSR, ssrData }) => {
-  const [err, setErr] = useState(false);
-  const [result, setResult] = useState({ loading: true, products: null });
-  useEffect(() => {
-    const getData = async () => {
-      const url = config.SSRApiStack.apiurl;
-      try {
-        let result = await axios.get(url);
-        setResult({ loading: false, products: result.data });
-      } catch (error) {
-        setErr(error);
-      }
-    };
-    getData();
-  }, []);
-  if (err) {
-    return <div>Error {err}</div>;
-  } else {
-    return (
-      <div>
-        <ProductList result={result} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <LearnBetterLife />
+    </div>
+  );
 };
 
 export default App;
