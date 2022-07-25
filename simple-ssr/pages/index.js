4a28d193daa201ea6e2ffab159373559705949dd
Yifeng Zhu <yifeng.zhu.mel@gmail.com>
2022-07-25 20:44:32 +1000

    updated ui

--- simple-ssr/pages/index.js
+++ simple-ssr/pages/index.js
@@ -4,6 +4,30 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Script from 'next/script';
+import { Container, Typography } from "@mui/material";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Divider from '@mui/material/Divider';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const LearnBetterLife = ({ result }) => {
   const [loanAmount, setLoanAmount] = useState();
@@ -93,87 +117,121 @@ const LearnBetterLife = ({ result }) => {
     `}
       </Script>
       <div>
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            required
-            id="filled-number"
-            label="Principal Loan Amount"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="standard"
-            onChange={e => setLoanAmount(e.target.value)}
-          />
-          <TextField
-            required
-            id="filled-number"
-            label="Annual Interest Rate"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="standard"
-            onChange={e => setInterest(e.target.value)}
-          />
-          <TextField
-            required
-            id="filled-number"
-            label="Term(years)"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="standard"
-            onChange={e => setTerm(e.target.value)}
-          />
-          <TextField
-            required
-            id="filled-number"
-            label="My Annual Income"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="standard"
-            onChange={e => setMySalary(e.target.value)}
-          />
-          <TextField
-            id="filled-number"
-            label="Partner Annual Income (optional)"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="standard"
-            onChange={e => setPartnerySalary(e.target.value)}
-          />
-          <div>
-            <Button variant="contained" onClick={calculate}>Calculate</Button>
-          </div>
-        </Box>
-        <Box>
-          <span>
-            Monthly Repayment: {repayment ? parseFloat(repayment.toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
-          </span>
-          <div>
-            Monthly Net Income: {netIncome ? parseFloat(netIncome.toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
-          </div>
-          <div>
-            Monthly Balance: {repayment && netIncome ? parseFloat((netIncome - repayment).toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
-          </div>
-          <div>
-            Total Payment: {repayment ? parseFloat((repayment * term * 12).toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
-          </div>
-        </Box>
+        <Container>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={4}>
+              <TextField
+                fullWidth
+                required
+                id="filled-number"
+                label="Principal Loan Amount"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+                onChange={e => setLoanAmount(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <TextField
+                fullWidth
+                required
+                id="filled-number"
+                label="Annual Interest Rate"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+                onChange={e => setInterest(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <TextField
+                required
+                fullWidth
+                id="filled-number"
+                label="Term(years)"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+                onChange={e => setTerm(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField
+                required
+                fullWidth
+                id="filled-number"
+                label="My Annual Income"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+                onChange={e => setMySalary(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField
+                fullWidth
+                id="filled-number"
+                label="Partner Annual Income (optional)"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+                onChange={e => setPartnerySalary(e.target.value)}
+              />
+            </Grid>
+            <Grid item container
+              direction="column"
+              alignItems="center"
+              justify="center"
+            >
+              <Grid>
+                <Button variant="contained" onClick={calculate}>Calculate</Button>
 
+              </Grid>
+              <Grid sx={{ mt: 1.5, minWidth: 300 }}>
+                <Card variant="outlined">
+                  <CardContent>
+                    <Typography color="text.secondary">
+                      Monthly Repayment:
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      {repayment ? parseFloat(repayment.toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
+                    </Typography>
+                    <Typography color="text.secondary">
+                      Monthly Net Income:
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      {netIncome ? parseFloat(netIncome.toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
+                    </Typography>
+                    <Typography color="text.secondary">
+                      Monthly Balance:
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      {repayment && netIncome ? parseFloat((netIncome - repayment).toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
+                    </Typography>
+                    <Typography color="text.secondary">
+                      Total Payment:
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      {repayment ? parseFloat((repayment * term * 12).toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
+        </Container>
       </div>
     </div >
   );
