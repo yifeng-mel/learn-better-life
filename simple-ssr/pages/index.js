22f696b1c908c3acf42b036f501369a744a2acf7
Xin Wang <xin_wang@racv.com.au>
2024-01-30 21:10:09 +1100

    add house value and ltv ration

--- simple-ssr/pages/index.js
+++ simple-ssr/pages/index.js
@@ -4,25 +4,37 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Script from 'next/script';
-import { Container, Typography } from "@mui/material";
+import { Container, FormLabel, InputLabel, Typography } from "@mui/material";
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
 const LearnBetterLife = ({ result }) => {
+  const [houseValue, setHouseValue] = useState();
+  const [lvr, setLvr] = useState(80);
   const [loanAmount, setLoanAmount] = useState();
-  const [interest, setInterest] = useState(3);
+  const [displayLoanAmount, setDisplayLoanAmount] = useState();
+  const [isCustomLoanAmount, setIsCustomLoanAmount] = useState();
+  const [interest, setInterest] = useState(6.6);
   const [term, setTerm] = useState(30);
+  const [downPayment, setDownPayment] = useState();
+  const [stampDuty, setStampDuty] = useState();
   const [repayment, setRepayment] = useState();
   const [mySalary, setMySalary] = useState();
   const [partnerSalary, setPartnerySalary] = useState();
   const [netIncome, setNetIncome] = useState();
 
+  const getReadableNumber = (inputNumber) => {
+    return inputNumber ? parseFloat(inputNumber.toFixed(2)).toLocaleString(undefined, { maximumFractionDigits: 2 }) : ''
+  }
+
   const calRepayment = () => {
+    const localLoanAmount = isCustomLoanAmount ? loanAmount : displayLoanAmount
+
     const monthlyRate = interest / 100 / 12
     const numberOfPayments = term * 12
 
-    const top = loanAmount * monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)
+    const top = localLoanAmount * monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)
     const bottom = Math.pow(1 + monthlyRate, numberOfPayments) - 1
     const result = Math.round(top / bottom)
 
@@ -71,8 +83,39 @@ const LearnBetterLife = ({ result }) => {
     return (annualSalary - calTax(annualSalary) - annualSalary * 0.02) / 12
   }
 
+  const calDisplayLoanAmount = (houseValue, lvr) => {
+    setDisplayLoanAmount(houseValue * lvr / 100)
+  }
+
+  const calDownPayment = () => {
+    if (houseValue !== '' && houseValue !== undefined && lvr !== '' && lvr !== undefined) {
+      setDownPayment(houseValue * (100 - lvr) / 100)
+    }
+  }
+
+  const calStampDuty = () => {
+    setStampDuty(getStampDuty())
+  }
+
+  const getStampDuty = () => {
+    if (houseValue < 25000) {
+      return houseValue * 1.4
+    } else if (houseValue >= 25000 && houseValue < 130000) {
+      return 350 + ((houseValue - 25000) * 2.4 / 100)
+    } else if (houseValue >= 130000 && houseValue < 960000) {
+      return 2870 + ( (houseValue - 130000) * 6 / 100 )
+    } else if (houseValue >= 960000 && houseValue < 2000000) {
+      return houseValue * 5.5 / 100
+    } else {
+      return 110000 + ((houseValue - 2000000) * 6.5 / 100)
+    }
+  }
 
   const calculate = () => {
+    setDownPayment(undefined)
+    setStampDuty(undefined)
+    calDownPayment()
+    calStampDuty()
     calRepayment()
     calNetIncome()
   }
@@ -110,24 +153,80 @@ const LearnBetterLife = ({ result }) => {
                   Mortgage and Salary Calculator (Australia)
                 </Typography>
               </Grid>
-
               <Grid item xs={12} sm={12} md={4}>
                 <TextField
+                  disabled = {isCustomLoanAmount ? true : false}
                   fullWidth
                   required
                   id="filled-number"
-                  label="Principal Loan Amount"
+                  label="House Value"
                   type="number"
                   InputLabelProps={{
                     shrink: true,
                   }}
                   variant="standard"
-                  onChange={e => setLoanAmount(e.target.value)}
+                  onChange={e => {
+                    if (e.target.value === '') {
+                      setIsCustomLoanAmount(true)
+                    } else {
+                      setIsCustomLoanAmount(false)
+                      calDisplayLoanAmount(e.target.value, lvr)
+                    }
+
+                    setHouseValue(e.target.value)
+                  }}
                 />
               </Grid>
               <Grid item xs={12} sm={12} md={4}>
                 <TextField
-                  defaultValue={3}
+                  disabled = {isCustomLoanAmount ? true : false}
+                  defaultValue={80}
+                  fullWidth
+                  required
+                  id="filled-number"
+                  label="Loan to Value Ratio (%)"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  onChange={e => setLvr(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                {
+                  isCustomLoanAmount === undefined || isCustomLoanAmount === true ? 
+                  <TextField
+                    fullWidth
+                    required
+                    id="filled-number"
+                    label="Principal Loan Amount"
+                    type="number"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="standard"
+                    onChange={e => {
+                      if (e.target.value === '') {
+                        setIsCustomLoanAmount(false)
+                      } else {
+                        setIsCustomLoanAmount(true)
+                      }
+
+                      setLoanAmount(e.target.value)
+                    }}
+                  /> : 
+                  <div>
+                    <InputLabel shrink={true}>Principal Loan Amount</InputLabel>
+                    <Typography color="body1" component="span">
+                      {getReadableNumber(displayLoanAmount)}
+                    </Typography>
+                  </div>
+                }
+              </Grid>
+              <Grid item xs={12} sm={12} md={4}>
+                <TextField
+                  defaultValue={6.6}
                   fullWidth
                   required
                   id="filled-number"
@@ -194,6 +293,15 @@ const LearnBetterLife = ({ result }) => {
                 <Grid sx={{ mt: 1.5, minWidth: 300 }} style={{ width: '100%' }}>
                   <Card variant="outlined">
                     <CardContent>
+                    <div>
+                        <Typography color="text.secondary" component="span">
+                          Upfront Cost (Down Payment + Stamp Duty): &nbsp;
+                          
+                        </Typography>
+                        <Typography variant="body1" component="span">
+                          {downPayment === undefined ? <div></div> : <div>{getReadableNumber(downPayment)} + {getReadableNumber(stampDuty)} = {getReadableNumber(downPayment + stampDuty)}</div>}
+                        </Typography>
+                      </div>
                       <div>
                         <Typography color="text.secondary" component="span">
                           Monthly Repayment: &nbsp;
